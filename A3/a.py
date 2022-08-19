6cb582d5201c283a1f7aa2e21230b69d87820703
VPB <vitorpossebontra@gmail.com>
2022-08-18 21:55:16 -0300

    A3

---
+++ A3/a.py
@@ -0,0 +1,18 @@
+#==============Vetor
+#numeros = ['0',]
+#x = int (input("Digite o numero: "))
+#numeros.append(x)
+#for i in numeros:
+#    print (i)
+#===================
+#
+# 1) Faça um Programa que peça dois números e imprima o maior deles.
+# 2) Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+# 3)Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
+#    -A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+#    -A mensagem "Reprovado", se a média for menor do que sete;
+#    -A mensagem "Aprovado com Distinção", se a média for igual a dez.
+#
+# 4) Faça um Programa que leia três números e mostre o maior deles.
+# 5) Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+# 6) Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.
---
+++ A3/exercicios.py
@@ -0,0 +1,51 @@
+# 1)nA = input ("Digite o primeiro numero: ")
+#   nB = input ("Digite o segundo numero: ")
+#   print(" ",nA," ",nB)
+#==================================================
+# 2) valorA = int (input ("Digite o valor: "))
+#   if valorA > 0:
+#       print("O valor é positivo")
+#   elif valorA < 0:
+#      print("O valor é negativo")
+#==================================================
+# 3)
+#print("Insira as notas separadamente")
+#n1 = float ( input ("Insira a nota: "))
+#n2 = float ( input ("Insira a nota: "))
+#media = (n1+n2)/2
+#if media == 10:
+#    print("Aprovado com Distinção")
+#elif media > 7 or media == 7:
+#    print("Aprovado")
+#elif media == 0:
+#    print("Voce tentou??")
+#elif media < 7:
+#    print("Reprovado")
+#====================================================
+# 4)
+#print ("Insira 3 numeros: ")
+#n1 = int (input ("Primeiro numero: "))
+#n2 = int (input ("Segundo numero: "))
+#n3 = int (input ("Terceiro numero:"))
+#if n1 > n2 and n1 > n3:
+#    print("1º",n1)
+#if n2 > n1 and n2 > n3:
+#    print("2º",n2)
+#if n3 > n1 and n3 > n2:
+#    print("º3",n3)
+# 5)
+#numerosInt= ['1','2','3','4','5']
+#for i in numerosInt:
+#    print(i)
+#6)
+from audioop import reverse
+
+
+numerosInt = []
+z = 0
+for z in range(10):
+    x = int (input("Digite o numero: "))
+    numerosInt.append(x)
+numerosInt.reverse()
+for x in numerosInt:
+    print (x)
