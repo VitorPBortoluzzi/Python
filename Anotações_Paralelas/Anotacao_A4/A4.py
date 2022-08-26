e97efb32927c72b30088c4a186ca50f1d70ddbd9
VPB <vitorpossebontra@gmail.com>
2022-08-25 21:38:57 -0300

    A4

--- Anotações_Paralelas/Anotacao_A4/A4.py
+++ Anotações_Paralelas/Anotacao_A4/A4.py
@@ -1,9 +1,9 @@
 frase = input("Digite sua demanda: ")
 print("Sua demanda é: "+frase)
-
-if ((frase.find("quero") != -1) or (frase.find("preciso") != -1 )or (frase.find("necessito")!= -1) or (frase.find("devo")!= -1)):
+frase = frase.upper()
+if ((frase.find("QUERO") != -1) or (frase.find("PRECISO") != -1 )or (frase.find("NECESSITO")!= -1) or (frase.find("DEVO")!= -1)):
     print("Demanda de emergência")
-elif(frase.find("ria") != -1):
+elif(frase.find("RIA") != -1):
     print ("Demanda de urgencia mas com atenção")
 else:
     print ("Relato sem demanda")
